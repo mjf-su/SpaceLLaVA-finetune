e05d1c9f1048e1d8391c7f1e64657e5cbfab7573
mjf-su <114531941+mjf-su@users.noreply.github.com>
2024-12-31 17:15:46 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,6 @@ pip install --upgrade pip
 pip install -e .
 pip install -e ".[train]"
 pip install flash-attn --no-build-isolation --no-cache-dir
+pip uninstall transformers
+pip install transformers[torch]
+pip install protobuf==3.20.*
